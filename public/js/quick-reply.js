22cd19b5ff4089744bbdf33a99b3f232f23c4297
vti <viacheslav.t@gmail.com>
2015-01-07 18:59:24 +0200

    fix js

--- public/js/quick-reply.js
+++ public/js/quick-reply.js
@@ -17,7 +17,30 @@
        $(this).removeClass('error');
     });
 
-    $('.quick-reply-button, .quick-edit-button').click(function() {
+    $('.quick-reply-button').click(function() {
+        var form = $(this).parent().find('.quick-reply-form');
+
+        if (!form.css('display') || form.css('display') == 'none') {
+            form.show();
+            form.find('textarea').focus();
+
+            form.find('textarea').keydown(function (e) {
+              if ((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey) {
+                  e.preventDefault();
+
+                  $(this).parent().parent().submit();
+              }
+            });
+        }
+        else {
+            form.find('textarea').off('keydown');
+            form.hide();
+        }
+
+        return false;
+    });
+
+    $('.quick-edit-button').click(function() {
         var form = $(this).parent().find('.quick-edit-form');
 
         if (!form.css('display') || form.css('display') == 'none') {
